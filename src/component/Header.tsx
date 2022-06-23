0c7d3766748545629ecc5ce95b62a82c10d64f84
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2022-06-22 22:44:04 -0500

    first commit

---
+++ src/component/Header.tsx
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar.tsx"
+const Header = () => {
+    return (
+        <Navbar />
+    )
+}
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "../component/Header.tsx";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Home;
